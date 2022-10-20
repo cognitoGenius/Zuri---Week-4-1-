1f1bc7d8a71cbbc4ac41fa07dbea280083d9207f
Ubongabasi Tommy <>
2022-10-20 22:30:45 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')();
+const username = prompt('What is your username?: ')
+
+//Core variable declarations
+let tries = 3, range = 2, stage = 1, score = 0
+
+//Controller function
+const guessController = function(range){
+  let value = Math.trunc(Math.random() * range + 1)
+  console.log(value)
+  console.log(`Hi ${username}, Welcome to stage ${stage}:`)
+  console.log(`In this stage, your guess range is between 1 and ${range}`)
+  let guess = +prompt('What is your guess?: ')
+  if(guess === value) {
+    score++
+    range++
+    stage++
+    guessController(range)
+  }else{
+    if(tries === 0){
+      console.log(`Ouch!, Game over, ${username}. Try again.\n You scored ${score} and got to stage ${stage}`)
+    }else{
+      tries--
+      console.log(`Wrong answer, you have ${tries} tries left`)
+      guessController(range)
+    }
+  }
+}
+guessController(range)
